350672c77177a8b168f9513a9d4d67171d2e3fed
Greta Gri <greta.klimaite@gmail.com>
2018-10-30 00:49:13 +0200

    Cyhanges to java files and added details activity plus layout.

---
+++ app/src/main/java/com/enpassio/androidmvpandmvvmpatterns/mvp_pattern/view/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.enpassio.androidmvpandmvvmpatterns.mvp_pattern.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.enpassio.androidmvpandmvvmpatterns.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/enpassio/androidmvpandmvvmpatterns/mvp_pattern/view/MainActivity.java
@@ -0,0 +1,29 @@
+package com.enpassio.androidmvpandmvvmpatterns.mvp_pattern.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.enpassio.androidmvpandmvvmpatterns.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.main_button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: show list in recyclerView with search results.
+            }
+        });
+    }
+}
