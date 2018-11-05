9339db54cf9e984174f49e366c7804a60bca6f33
enpassio <abhishekraj@abhishekraj.me>
2018-11-05 23:34:40 +0530

    Added classes to do network operation

---
+++ app/src/main/java/mvvm_pattern/mvvmbyabhi/data/network/NewsApiService.java
@@ -0,0 +1,16 @@
+package mvvm_pattern.mvvmbyabhi.data.network;
+
+import mvvm_pattern.mvvmbyabhi.data.model.NewsResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsApiService {
+
+    /**
+     * Retrieve list of articles
+     */
+    @GET("v2/top-headlines")
+    Call<NewsResponse> getNewsArticles(@Query("apiKey") String publicKey,
+                                       @Query("q") String searchQuery);
+}
