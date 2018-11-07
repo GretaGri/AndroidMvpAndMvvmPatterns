a8bdd1177cc92d906d2db608c407cbae2b4dd59d
enpassio <abhishekraj@abhishekraj.me>
2018-11-07 23:32:20 +0530

    Added adapter setup

--- app/src/main/java/mvvm_pattern/mvvmbyabhi/view/MainActivity.java
+++ app/src/main/java/mvvm_pattern/mvvmbyabhi/view/MainActivity.java
@@ -5,12 +5,15 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.enpassio.androidmvpandmvvmpatterns.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import mvvm_pattern.mvvmbyabhi.data.model.Article;
@@ -21,10 +24,27 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     private ArticlesViewModel articlesViewModel;
 
+    /* Adapters for inflating different recyclerview */
+    NewsAdapter mNewsAdapter;
+    /* Set layout managers on those recycler views */
+    LinearLayoutManager newslayoutmanager;
+    RecyclerView mNewsrecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewsrecyclerView = findViewById(R.id.main_recycler_view);
+        /*
+         * Setup layout manager for items with orientation
+         * Also supports `LinearLayoutManager.HORIZONTAL`
+         */
+        newslayoutmanager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        /* Attach layout manager to the RecyclerView */
+        mNewsrecyclerView.setLayoutManager(newslayoutmanager);
+        mNewsrecyclerView.setAdapter(mNewsAdapter);
 
         button = findViewById(R.id.main_button);
         articlesViewModel = ViewModelProviders.of(this).get(ArticlesViewModel.class);
@@ -38,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onChanged(@Nullable List<Article> articles) {
                                 Log.d("my_tag", "size of articles received is: " + articles.size());
+                                mNewsAdapter.onNewData((ArrayList<Article>) articles);
                             }
                         });
             }
