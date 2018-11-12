b67783afffd60bc7cb676b8d65594d7436a87d50
enpassio <abhishekraj@abhishekraj.me>
2018-11-13 04:14:35 +0530

    Added shimmer effect

--- app/src/main/java/com/enpassio/androidmvpandmvvmpatterns/MvpByAbhi/view/ArticlePagedListAdapter.java
+++ app/src/main/java/com/enpassio/androidmvpandmvvmpatterns/MvpByAbhi/view/ArticlePagedListAdapter.java
@@ -3,16 +3,24 @@ package com.enpassio.androidmvpandmvvmpatterns.MvpByAbhi.view;
 
 import android.arch.paging.PagedListAdapter;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.enpassio.androidmvpandmvvmpatterns.MvpByAbhi.GlideApp;
 import com.enpassio.androidmvpandmvvmpatterns.MvpByAbhi.data.model.Article;
 import com.enpassio.androidmvpandmvvmpatterns.R;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class ArticlePagedListAdapter extends PagedListAdapter<Article, RecyclerView.ViewHolder> {
 
@@ -52,6 +60,8 @@ public class ArticlePagedListAdapter extends PagedListAdapter<Article, RecyclerV
         final TextView newsAuthorTextView;
         final TextView newsPublishingDateTextView;
         final ImageView newsPosterImageView;
+        final ShimmerFrameLayout container;
+
 
         /*
         We also create a constructor that accepts the entire item row
@@ -67,6 +77,7 @@ public class ArticlePagedListAdapter extends PagedListAdapter<Article, RecyclerV
             newsAuthorTextView = itemView.findViewById(R.id.list_item_author);
             newsPublishingDateTextView = itemView.findViewById(R.id.list_item_published_date);
             newsPosterImageView = itemView.findViewById(R.id.list_item_image);
+            container =(ShimmerFrameLayout) itemView.findViewById(R.id.shimmer_view_container);
         }
 
         void bindTo(Article article) {
@@ -78,11 +89,25 @@ public class ArticlePagedListAdapter extends PagedListAdapter<Article, RecyclerV
 
                 newsPublishingDateTextView.setText(""+dateArray[0]+" "+dateArray[1].subSequence(0, dateArray[1].length()-1));
                 newsAuthorTextView.setText(article.getUrl());
+                container.startShimmer();
                 GlideApp
                         .with(mContext)
                         .load(article.getUrlToImage())
                         .centerCrop()
+                        .addListener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                container.stopShimmer();
+                                return false;
+                            }
+                        })
                         .into(newsPosterImageView);
+
             }
         }
     }
