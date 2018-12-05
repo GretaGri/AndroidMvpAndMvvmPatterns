3deb1c3791fc6e9a688fbd8f0f7c37baa199c673
enpassio <abhishekraj@abhishekraj.me>
2018-12-05 20:07:54 +0530

    Added FavoriteActivity

---
+++ app/src/main/java/com/enpassio/androidmvpandmvvmpatterns/mvvmbyabhi/view/FavoriteActivity.java
@@ -0,0 +1,16 @@
+package com.enpassio.androidmvpandmvvmpatterns.mvvmbyabhi.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.enpassio.androidmvpandmvvmpatterns.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
